9c62fd6b239ab8d35b0deb31f6f923038ad1d553
Dworv <dwarvyt@gmail.com>
2022-05-26 22:35:51 -0700

    ref: change how base.py and extension.py stuctured

--- interactions/ext/persistence/base.py
+++ interactions/ext/persistence/base.py
@@ -1,14 +1,5 @@
-from types import MethodType
-from interactions import (
-    Client,
-    ComponentContext,
-    CommandContext,
-    Extension,
-    extension_listener,
-)
 from interactions.ext import Base, Version
-from .listener import persistent_component, persistent_modal
-from .parse import PersistentCustomID
+from .extension import Persistence
 
 version = Version(version="0.1.0")
 
@@ -19,25 +10,6 @@ base = Base(
     description="An extension to add simple custom_id encoding to interactions.py.",
     long_description=".",  # TODO
 )
-
-
-class Persistence(Extension):
-    def __init__(self, bot: Client):
-        bot.persistent_component = MethodType(persistent_component, bot)
-        bot.persistent_modal = MethodType(persistent_modal, bot)
-
-    @extension_listener(name="on_component")
-    async def _on_component(self, ctx: ComponentContext):
-        if not ctx.custom_id.startswith("persistence_"):
-            return
-        custom_id = PersistentCustomID.from_string(ctx.custom_id)
-        listener = self.client._websocket._dispatch
-        for name, funcs in listener.events.items():
-            if name == "component_persistence_" + custom_id.tag:
-                for func in funcs:
-                    await func(ctx, custom_id.package)
-                break
-
 
 def setup(bot):
     return Persistence(bot)
