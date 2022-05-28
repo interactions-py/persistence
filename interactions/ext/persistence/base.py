bc5ab1c0592eb74a3ba6a5d3a6942ea4e1916f70
Dworv <dwarvyt@gmail.com>
2022-05-28 09:40:07 -0700

    chore: version bump to 1.0.0

--- interactions/ext/persistence/base.py
+++ interactions/ext/persistence/base.py
@@ -1,14 +1,14 @@
 from interactions.ext import Base, Version
 from .extension import Persistence
 
-version = Version(version="0.1.0")
+version = Version(version="1.0.0-beta.1")
 
 base = Base(
     name="Persistence",
     version=version,
     link=f"https://github.com/dworv/interactions-persistence",
     description="An extension to add simple custom_id encoding to interactions.py.",
-    long_description=".",  # TODO
+    packages="interactions.ext.persistence"
 )
 
 def setup(bot):
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="interactions-persistence",
-    version="0.1.0",
+    version="1.0.0",
     description="Encode objects in custom_ids.",
     long_description=long_description,
     long_description_content_type="text/markdown",
