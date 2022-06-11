c4f03e24a4678d0e00c7f91f3b5b0d103701c743
Dworv <dwarvyt@gmail.com>
2022-06-11 11:02:25 -0700

    ref: abstract key generation to cipher file

--- interactions/ext/persistence/scripts/keygen.py
+++ interactions/ext/persistence/scripts/keygen.py
@@ -1,8 +1,5 @@
 from random import choice
+from ..cipher import generate_key
 
-HEX_ALPHABET = "0123456789ABCDEF"
 def generate_key(): 
-    print(
-    "Random 32 character key: " +
-    "".join(choice(HEX_ALPHABET) for _ in range(32))
-    )
+    print(generate_key())
