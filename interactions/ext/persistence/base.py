db34143978d0b45c500b886132c851e40ced4b7e
Dworv <88643996+Dworv@users.noreply.github.com>
2022-07-24 21:49:53 -0700

    chore: bump version to 2.2.1

--- interactions/ext/persistence/base.py
+++ interactions/ext/persistence/base.py
@@ -4,7 +4,7 @@ from interactions.ext import Base, Version, VersionAuthor
 from .persistence import Persistence
 
 version = Version(
-    version="2.2.0",
+    version="2.2.1",
     author=VersionAuthor(
         name="Dworv",
         email="dwarvyt@gmail.com",
